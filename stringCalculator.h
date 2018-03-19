017ebd1e8aabf3c69d591907d659248f666ccd42
Corey Gray <csgray@alaska.edu>
2018-03-19 00:04:03 -0800

    Passes 'Two numbers, newline delimited, returns the sum.' test.

--- stringCalculator.h
+++ stringCalculator.h
@@ -1,12 +1,14 @@
-#include <string>	// std::getline, std::string, std::stoi (converts strings to integers!)
-#include <sstream>	// std::istringstream
+#include <string>		// std::getline, std::string, std::stoi (converts strings to integers!)
+#include <sstream>		// std::istringstream
+#include <algorithm>	// std::replace
 
 int add(std::string numbers) {
 	int sum = 0;
 	int number;
-	std::istringstream stream(numbers);
 	std::string token;
 	
+	std::replace(numbers.begin(), numbers.end(), '\n', ',');
+	std::istringstream stream(numbers);
 	while (std::getline(stream, token, ','))
 		sum += std::stoi(token);
 
