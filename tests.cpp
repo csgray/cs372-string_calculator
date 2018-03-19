2cee075266208c0941950a66285a96cd0c8baaa6
Corey Gray <csgray@alaska.edu>
2018-03-18 23:06:18 -0800

    Add 'Tow numbers, comma delimited, returns the sum.' test.

--- tests.cpp
+++ tests.cpp
@@ -10,3 +10,9 @@ TEST_CASE("A single number returns the value.") {
 	REQUIRE(add("10") == 10);
 	REQUIRE(add("101") == 101);
 }
+
+TEST_CASE("Two numbers, comma delimited, returns the sum.") {
+	REQUIRE(add("2,4") == 6);
+	REQUIRE(add("13,39") == 52);
+	REQUIRE(add("240,601") == 841);
+}
